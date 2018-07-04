30f0133c37cba3a5f86dc9cffac6266ae6b46dbb
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-07-04 15:57:33 -0300

    remove BITPRIM_BUILD_NUMBER envvar

--- conanfile.py
+++ conanfile.py
@@ -100,7 +100,7 @@ class BitprimDatabaseConan(ConanFile):
         if self.settings.arch == "x86_64" and self.options.microarchitecture == "_DUMMY_":
             del self.options.fix_march
             # self.options.remove("fix_march")
-            raise Exception ("fix_march option is for using together with microarchitecture option.")
+            # raise Exception ("fix_march option is for using together with microarchitecture option.")
 
         if self.settings.arch == "x86_64":
             march_conan_manip(self)
@@ -138,7 +138,7 @@ class BitprimDatabaseConan(ConanFile):
             cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " /DBOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE"
 
         cmake.definitions["MICROARCHITECTURE"] = self.options.microarchitecture
-        cmake.definitions["BITPRIM_PROJECT_VERSION"] = get_version()
+        cmake.definitions["BITPRIM_PROJECT_VERSION"] = self.version
 
         if self.settings.compiler == "gcc":
             if float(str(self.settings.compiler.version)) >= 5:
@@ -151,7 +151,6 @@ class BitprimDatabaseConan(ConanFile):
 
         pass_march_to_compiler(self, cmake)
 
-        cmake.definitions["BITPRIM_BUILD_NUMBER"] = os.getenv('BITPRIM_BUILD_NUMBER', '-')
         cmake.configure(source_dir=self.source_folder)
         cmake.build()
 
